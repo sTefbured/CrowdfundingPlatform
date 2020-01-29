ab57dd2cec9fd796464f9ec846056c2c3c65f6f7
sTefbured <stef.for.ever2000@gmail.com>
2020-01-30 01:54:31 +0300

    Add Campaign and Category models

---
+++ Data/Models/Campaign.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdfundingPlatform.Data.Models
+{
+    public class Campaign
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime GoalDate { get; set; }
+        public double MoneyEarned { get; set; }
+        public double MoneyGoal { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +17,7 @@ namespace CrowdfundingPlatform
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore(options => options.EnableEndpointRouting = false); //TODO: MAYBE CHANGE TO AddMvc()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +28,9 @@ namespace CrowdfundingPlatform
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
